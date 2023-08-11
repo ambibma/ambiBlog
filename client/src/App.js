5904ddcc6b7533204cd6c9adac0ccffd32edc2f5
ambi hidalgo <ambio.pk@gmail.com>
2023-08-10 18:30:02 -0700

    Refactor App with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
 
 import './App.css';
 import Header from './components/Header';
+import Layout from './components/Layout';
 import Post from './components/Post';
 import {Routes, Route} from 'react-router-dom'
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <Routes>
-      <Route index element={
-        <main>  
-          <Header/>
-          <Post/>
-          <Post/>
-          <Post/>      
-        </main>
-      } />
-      <Route path={'/login'} element={
-        <div>login</div>
-      }/>
+      <Route path="/" element={<Layout/>}>  
+        <Route index element={<IndexPage/>}/>
+        <Route path={'/login'} element={<LoginPage/>}/>
+        <Route path={'/register'} element={<RegisterPage/>}/>
+        </Route>
     </Routes>
     
   );
