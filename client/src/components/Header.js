0e87a93804bf194b46ed520eb80b0c924151a5b9
ambi hidalgo <ambio.pk@gmail.com>
2023-08-11 15:56:46 -0700

    Add create profile off auth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
 export default function Header() {
+  const [username, setUsername] = useState(null)
+  useEffect(()=> {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include'
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUsername(userInfo.username);
+      })
+    })
+
+  }, [])
+  function handleLogout () {
+      fetch('http://localhost:4000/logout', {
+        credentials: 'include',
+        method: 'POST',
+      })
+  }
+
   return (
     <header >
         <Link to= "/" className = "logo"> My Blog</Link>
         <nav>
+          {username && (
+            <>
+            <Link to="/create">create New Post</Link>
+            <a onClick={handleLogout}>Logout</a>
+            </>
+          )}
+            {!username &&(
+              <>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+              </>
+            )}
+        
           <Link to="/login">Login</Link>
           <Link to="/register">Register</Link>        
         </nav>
