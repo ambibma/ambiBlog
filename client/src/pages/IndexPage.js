8d77b459f2e79499c81d1ae25c0ade8babf81800
ambi hidalgo <ambio.pk@gmail.com>
2023-08-10 18:29:37 -0700

    Create indexPage comp

---
+++ client/src/pages/IndexPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Post from '../components/Post'
+
+export default function IndexPage() {
+  return (
+    <>
+    <Post/>
+    <Post/>
+    <Post/>
+    </>
+  )
+}
