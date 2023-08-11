147e9a43ca7710cf35f5ca2dbdacc741b04757d8
ambi hidalgo <ambio.pk@gmail.com>
2023-08-10 18:02:30 -0700

    Add Header and Nav into Layout comp

---
+++ client/src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import { Outlet } from 'react-router-dom'
+
+export default function Layout() {
+  return (
+   <main>
+    <Header/>
+    <Outlet/>
+    
+   </main>
+  )
+}
