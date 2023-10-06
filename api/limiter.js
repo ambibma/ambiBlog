c27b24708e742cdc264e278a8c56176c750d196d
ambi hidalgo <ambio.pk@gmail.com>
2023-10-05 19:27:29 -0700

    Update Limiter config

--- api/limiter.js
+++ api/limiter.js
@@ -7,6 +7,8 @@ const limiter = rateLimit({
   windowMs:  10* 60 * 1000, // 15 minutes
   max: 5, // Max requests per windowMs
   message: 'Too many requests from this IP, please try again later.',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
 module.exports = limiter;
