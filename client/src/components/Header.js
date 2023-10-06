07f9bfe76fcdaaaba4455da97dc02f69eb29ab59
ambi hidalgo <ambio.pk@gmail.com>
2023-10-05 19:42:45 -0700

    Update url

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://ambi-blog.onrender.com/profile", {
+    fetch("https://ambi-blog-api.onrender.com/profile", {
       credentials: "include",
     })
       .then((response) => {
