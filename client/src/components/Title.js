4e8916c312b91ad23954ee1e22c7b96b87c4548e
ambi hidalgo <ambio.pk@gmail.com>
2024-02-02 21:36:36 -0800

    Add conditional rendering to accept blog posts and project data

--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -1,13 +1,44 @@
+import {format} from 'date-fns';
+
+function Title({title, tech, link, description, date}) {
 
 
-function Title({title, link, date}) {
   
   return (
-    <div>
-      <a href={link} >{title}</a>
-      <h3>{date}</h3>
+    <>
+    <div className="titleClass">
+      <>
+        {tech ? (
+          <>
+            <a className='titleLink' href={link}>{title} - <span className="titleTech">{tech}</span></a>
+          </>
+
+
+        ) : (
+          <>
+          <a className='titleLink' href={link}>{title}</a>
+          </>
+
+        )} 
+        <p className="titleDescription">{description}</p>
+        {date ? (
+          <>
+             <p><time>{format(new Date(date), 'MMM d yyyy H:MM')}</time></p>
+          </>
+
+
+        ) : (
+          <>
+         
+          </>
+
+        )} 
+        
+       </>
     </div>
+    </>
   );
 }
 
 export default Title;     
+
