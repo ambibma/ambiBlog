d1401eeae2772b7bd8f31c3bb166490b25cbc4a9
ambi hidalgo <ambio.pk@gmail.com>
2024-02-02 21:36:46 -0800

    Add project data

---
+++ client/src/Data/ProjectData.js
@@ -0,0 +1,49 @@
+
+
+export const ProjectData = [
+{
+  id: '1',
+  title: 'Jam Mate',
+  tech: 'React Native & Firebase',
+  link: 'https://github.com/ambibma/Capstone',
+  description: "React Native and Firebase social mobile matchmaking application in which musicians and producers can meet",
+},
+
+{
+  id: '2',
+  title: 'Card Shuffler ',
+  tech: 'Python & tkinter',
+  link: 'https://github.com/ambibma/card_shuffler',
+  description: 'A program that shuffles and deals cards, a stepping-project to my Reshuffler program that will recursively shuffle cards until it goes back into new deck order',
+},
+
+{
+  id: '3',
+  title: 'Open Quest',
+  tech: 'Javascript & Node.js Open AI API',
+  link: 'https://github.com/ambibma/teamWeek-1',
+  description: "A choose your own generative text adventure game. Players enter original themes in which the Davinci-3 language model and DALLE work in tandem to generate a unique adventure",
+},
+{
+  id: '4',
+  title: 'ExchangeMe',
+  tech: 'JS, Node.js',
+  link: 'https://ambibma.github.io/exchangeMe/',
+  description: 'An application using exchangerate api to convert USD into any world currency.',
+},
+  {
+  id: '5',
+  title: 'Sudoku Checker',
+  tech: 'JS, Node and Webpack',
+  link: 'https://ambibma.github.io/sudokuChecker/',
+  description: 'Sudoku Checker developed using Test Driven Development, just paste or enter your sudoku puzzle to verify!',
+  },
+{
+  id: '6',
+  title: 'Rabby and Turty',
+  tech: 'C# and Unity',
+  link: 'https://github.com/ambibma/rabby_and_turty_adventure',
+  description: ' An original game about a rabbit and turtle that must escape a mysterious facility with original art, animations and music.',
+},
+]
+
