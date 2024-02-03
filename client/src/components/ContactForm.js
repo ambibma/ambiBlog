953f7e119dccf5625da8cf90a3b5bdaf0392f1b0
ambi hidalgo <ambio.pk@gmail.com>
2024-02-02 23:28:40 -0800

    Add custom button to contact form

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -23,14 +23,19 @@ export const ContactForm = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
+    <form className="contact-form-container"ref={form} onSubmit={sendEmail}>
       <label>Name</label>
       <input type="text" name="user_name" />
       <label>Email</label>
       <input type="email" name="user_email" />
       <label>Message</label>
       <textarea name="message" />
-      <input type="submit" value="Send" />
+      <button className="custom-button" type="submit">
+        Send
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4"> {/* Adjusted size */}
+      <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+  </svg>
+</button>
     </form>
   );
 };
